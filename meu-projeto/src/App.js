d64c2e7ae5a83428e6c2734ec256655918f13cfc
elucasrg <lrggbi@gmail.com>
2024-03-27 16:30:23 -0300

    props

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,13 +1,23 @@
 
 import './App.css';
-import HelloWorld from './components/HelloWorld';
+
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
-
+  const nome = 'Iasmim'
   return (
     <div className="App">
     
-    <HelloWorld />
+    
+    <SayMyName nome="Lucas"/>
+    <SayMyName nome="Teobaldo"/>
+    <SayMyName nome={nome}/>
+    <Pessoa 
+    nome="Lucas" 
+    idade=" 25" 
+    profissao=" Programador" 
+    foto="https://via.placeholder.com/150"/>
     </div>
   );
 }
