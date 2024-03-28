3b3ec571093884d472e84f19eff955dbfd82be2c
elucasrg <lrggbi@gmail.com>
2024-03-28 16:32:24 -0300

    avançando em props

--- meu-projeto/src/components/List.js
+++ meu-projeto/src/components/List.js
@@ -4,9 +4,9 @@ function List () {
         <>
             <h1>Minha lista</h1>
             <ul>
-                <Item marca="Aple"/>
-                <Item marca="Sansumg"/>
-                <Item marca="Xaiomi"/>
+                <Item marca="Aple" ano_lancamento={2005}/>
+                <Item marca="Sansumg" ano_lancamento={2002}/>
+                <Item marca="Xaiomi" ano_lancamento={2012}/>
             </ul>
         </>
     )
