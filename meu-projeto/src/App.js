430b25700943810b5d651c4a4401b3bbc55c7e02
elucasrg <lrggbi@gmail.com>
2024-04-18 11:14:40 -0300

    condicional

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import Evento from './components/Evento'
-import Form from './components/Form'
+import Condicional from './components/Condicional';
 function App() {
   return (
     <div className="App">
-      <h1>Testatndo Eventos</h1>
-      <Evento numero="1"/>
-      <Form />
+      <h1>Renderização Condicional</h1>
+      <Condicional />
     </div>
   )
 }
